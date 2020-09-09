39a141f4eed52dcdbbd9fe7b975b8f7c5f199704
David Aniceto <david.aniceto@ctw.bmwgroup.com>
2020-09-10 00:43:04 +0100

    [myPetFinderApp API]
    refactoring and adding new generic logic

--- src/main/java/github/davids13/mypetfinderapp/control/dao/GenericDAO.java
+++ src/main/java/github/davids13/mypetfinderapp/control/dao/GenericDAO.java
@@ -1,5 +1,7 @@
 package github.davids13.mypetfinderapp.control.dao;
 
+import github.davids13.mypetfinderapp.commons.jpa.AbstractEntity;
+
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -15,11 +17,11 @@ public class GenericDAO {
     private EntityManager entityManager;
 
     // Generic CRUD methods
-    public <T> void save(final T object) {
+    public <T extends AbstractEntity> void save(final T object) {
         entityManager.persist(object);
     }
 
-    public Stream<?> getAll(final String queryName, Class<?> clazz) {
+    public Stream<?> getAll(final String queryName, final Class<?> clazz) {
         TypedQuery<?> query = entityManager.createNamedQuery(queryName, clazz);
         return query.getResultStream();
     }
