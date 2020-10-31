32b447b89d5875996468e8b509c6e9cc6bf32cfc
David Aniceto <David.Aniceto@ctw.bmwgroup.com>
2020-10-31 01:16:38 +0000

    [myPetFinderApp API]
    [> refactor service layer]

--- src/main/java/github/davids13/mypetfinderapp/control/service/PetService.java
+++ src/main/java/github/davids13/mypetfinderapp/control/service/PetService.java
@@ -1,5 +1,6 @@
 package github.davids13.mypetfinderapp.control.service;
 
+import github.davids13.mypetfinderapp.commons.jpa.AbstractEntity;
 import github.davids13.mypetfinderapp.control.dao.GenericDAO;
 import github.davids13.mypetfinderapp.entity.Owner;
 
@@ -14,14 +15,22 @@ public class PetService {
     @Inject
     private GenericDAO genericDAO;
 
+    public <T extends AbstractEntity> void create(final T t) {
+        genericDAO.save(t);
+    }
+
     // Owner
-    public void saveOwner(final Owner owner) {
-        genericDAO.save(owner);
+    public Owner find(final Integer id) {
+        if (id == null) {
+            return null;
+        }
+
+        return genericDAO.findById(Owner.class, id);
     }
 
     @SuppressWarnings("unchecked")
-    public List<Owner> getOwnersList() {
-        final Stream<Owner> ownerStream = (Stream<Owner>) genericDAO.getAll(Owner.OWNER_FIND_ALL, Owner.class);
+    public List<Owner> findAll() {
+        final Stream<Owner> ownerStream = (Stream<Owner>) genericDAO.findAll(Owner.OWNER_FIND_ALL, Owner.class);
         if (ownerStream == null)
             return Collections.emptyList();
 
