579623ae6c10ff2097055c8971fb6fc3bdf7b400
David Aniceto <David.Aniceto@ctw.bmwgroup.com>
2020-10-31 01:12:31 +0000

    [myPetFinderApp API] -m [> refactor resource]

--- src/main/java/github/davids13/mypetfinderapp/boundary/PetResource.java
+++ src/main/java/github/davids13/mypetfinderapp/boundary/PetResource.java
@@ -17,27 +17,41 @@ public class PetResource {
     @Inject
     private PetService petService;
 
-    // Owner
     @GET
     @Path("/owners")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getOwners() {
-        final List<Owner> ownerList = petService.getOwnersList();
+    public Response getAllOwners() {
+        final List<Owner> ownerList = petService.findAll();
         if (ownerList.isEmpty())
-            return Response.status(Response.Status.NO_CONTENT).build();
+            return Response.status(Response.Status.NOT_FOUND).build();
 
         return Response.ok(ownerList).build();
     }
 
+    @GET
+    @Path("/owner/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getOwner(@PathParam("id") Integer id) {
+        //LOG.log(Level.FINE, "REST request to get Person : {0}", id);
+        Owner owner = null;
+        if (id == null) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        } else {
+            owner = petService.find(id);
+        }
+
+        return Response.status(Response.Status.OK).build();
+    }
+
     @POST
-    @Path("/saveOwner")
+    @Path("/createOwner")
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response saveOwner(final Owner owner) {
+    public Response createOwner(final Owner owner) {
         if (owner.getFirstName().isEmpty() || owner.getLastName().isEmpty() || owner.getEmail().isEmpty() || owner.getPhone().isEmpty())
-            throw new WebApplicationException((Response.status(Response.Status.BAD_REQUEST).build()));
+            throw new WebApplicationException((Response.status(Response.Status.NOT_ACCEPTABLE).build()));
 
-        petService.saveOwner(owner);
+        petService.create(owner);
 
         return Response.status(Response.Status.CREATED).entity(owner).build();
     }
