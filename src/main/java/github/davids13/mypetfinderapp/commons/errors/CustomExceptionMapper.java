bd55c60d3952338b03e7bef44aa08fbe3d63c263
David Aniceto <David.Aniceto@ctw.bmwgroup.com>
2020-12-25 20:39:21 +0000

    [myPetFinderApp API] > added CustomExceptionMapper class

---
+++ src/main/java/github/davids13/mypetfinderapp/commons/errors/CustomExceptionMapper.java
@@ -0,0 +1,17 @@
+package github.davids13.mypetfinderapp.commons.errors;
+
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.Provider;
+
+@Provider // this tells the JAX-RS runtime that it is a component
+public class CustomExceptionMapper implements ExceptionMapper<CustomException> {
+
+    @Override
+    public Response toResponse(CustomException exception) {
+
+        return Response.status(Response.Status.NOT_FOUND).entity(new CustomException("exception.getMessage()", exception.getRequestDateTime(), exception.getStatusCode(), exception.getPetErrorCode(),
+                exception.getPetErrorDescription(), exception.getLink())).type(MediaType.APPLICATION_JSON).build();
+    }
+
+}
