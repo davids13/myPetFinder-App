5abcd3c676111c543c0038672edad44e338c1994
David Aniceto <David.Aniceto@ctw.bmwgroup.com>
2020-12-25 21:24:35 +0000

    testing

---
+++ src/main/java/github/davids13/mypetfinderapp/commons/cookies/Cookie.java
@@ -0,0 +1,43 @@
+package github.davids13.mypetfinderapp.commons.cookies;
+
+public class Cookie {
+
+    private String name;
+    private String value;
+    private String path;
+    private String domain;
+    private int version;
+
+    public Cookie(String name, String value, String path, String domain, int version) {
+        this.name = name;
+        this.value = value;
+        this.path = path;
+        this.domain = domain;
+        this.version = version;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public String getDomain() {
+        return domain;
+    }
+
+    public int getVersion() {
+        return version;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Cookie{name='%s', value='%s', path='%s', domain='%s', version=%d}", name, value, path, domain, version);
+    }
+}
