b51ba733d89b198ac8acf82c354140d3a0ddab75
David Aniceto <David.Aniceto@ctw.bmwgroup.com>
2020-12-25 20:56:09 +0000

    [myPetFinderApp API] > added more options into PetErrorDescription

--- src/main/java/github/davids13/mypetfinderapp/commons/errors/type/PetErrorDescription.java
+++ src/main/java/github/davids13/mypetfinderapp/commons/errors/type/PetErrorDescription.java
@@ -3,7 +3,8 @@ package github.davids13.mypetfinderapp.commons.errors.type;
 public enum PetErrorDescription {
 
     PET_ERROR_1("Owner with this specific id=[null] was not found"),
-    PET_ERROR_2("Pet with this specific id=[null] was not found");
+    PET_ERROR_2("Pet with this specific id=[null] was not found"),
+    PET_ERROR_3("You don't have the permission to delete this");
 
     public final String errorDescription;
 
