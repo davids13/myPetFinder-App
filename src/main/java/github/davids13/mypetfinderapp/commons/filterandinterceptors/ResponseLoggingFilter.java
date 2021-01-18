2998ee23bfcdca4b626859778e9e45aaeef07233
David Aniceto <David.Aniceto@ctw.bmwgroup.com>
2021-01-18 18:07:48 +0000

    [myPetFinderApp API] > Added Response Logging Filter class

---
+++ src/main/java/github/davids13/mypetfinderapp/commons/filterandinterceptors/ResponseLoggingFilter.java
@@ -0,0 +1,65 @@
+package github.davids13.mypetfinderapp.commons.filterandinterceptors;
+
+import org.apache.log4j.Logger;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.ext.Provider;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Provider
+public class ResponseLoggingFilter implements ContainerResponseFilter {
+
+    private static final Logger LOG = Logger.getLogger(ResponseLoggingFilter.class);
+
+    @Context
+    UriInfo info;
+
+    @Override
+    public void filter(ContainerRequestContext containerRequestContext, ContainerResponseContext containerResponseContext) {
+        final Object object = containerResponseContext.getEntity();
+        final int status = containerResponseContext.getStatus();
+
+        checkStatus(status, object);
+    }
+
+    private void checkStatus(final int status, final Object object) {
+        final Set<Integer> statusCode = new HashSet<>();
+        statusCode.add(400);
+        statusCode.add(404);
+
+        if (statusCode.contains(status))
+            LOG.error("Response: " + object.toString());
+    }
+
+    public UriInfo getInfo() {
+        return info;
+    }
+
+    public void setInfo(UriInfo info) {
+        this.info = info;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ResponseLoggingFilter that = (ResponseLoggingFilter) o;
+        return Objects.equals(info, that.info);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(info);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("ResponseLoggingFilter{info=%s}", info);
+    }
+}
