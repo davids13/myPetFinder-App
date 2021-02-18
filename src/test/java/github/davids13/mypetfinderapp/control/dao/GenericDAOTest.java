a18c720a2a72cbf54464c0475443efb9f6b935bb
David Aniceto <David.Aniceto@ctw.bmwgroup.com>
2021-02-18 22:49:45 +0000

    [myPetFinderApp API]
    > added git ignore

---
+++ src/test/java/github/davids13/mypetfinderapp/control/dao/GenericDAOTest.java
@@ -0,0 +1,50 @@
+package github.davids13.mypetfinderapp.control.dao;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GenericDAOTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void testEquals() {
+    }
+
+    @Test
+    void testHashCode() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
