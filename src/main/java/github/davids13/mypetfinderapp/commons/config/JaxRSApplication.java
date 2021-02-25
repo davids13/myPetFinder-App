8f81f9b85af35de77cdf6515a05f27340543ef53
David Aniceto <David.Aniceto@ctw.bmwgroup.com>
2021-02-25 21:00:05 +0000

    [myPetFinderApp API] > Added provider classes

--- src/main/java/github/davids13/mypetfinderapp/commons/config/JaxRSApplication.java
+++ src/main/java/github/davids13/mypetfinderapp/commons/config/JaxRSApplication.java
@@ -1,5 +1,8 @@
 package github.davids13.mypetfinderapp.commons.config;
 
+import github.davids13.mypetfinderapp.commons.errors.CustomExceptionMapper;
+import github.davids13.mypetfinderapp.commons.filterandinterceptors.RequestLoggingFilter;
+import github.davids13.mypetfinderapp.commons.filterandinterceptors.ResponseLoggingFilter;
 import io.swagger.v3.oas.annotations.ExternalDocumentation;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
@@ -10,6 +13,8 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
 
 @OpenAPIDefinition(
         info = @Info(
@@ -37,8 +42,14 @@ import javax.ws.rs.core.Application;
 @ApplicationPath("api")
 public class JaxRSApplication extends Application {
 
-    public String toString() {
-        return String.format("JaxRSApplication{} %s", super.toString());
+    // add provider classes here
+    @Override
+    public Set<Class<?>> getClasses() {
+        final Set<Class<?>> classes = new HashSet<>();
+        classes.add(CustomExceptionMapper.class);
+        classes.add(RequestLoggingFilter.class);
+        classes.add(ResponseLoggingFilter.class);
+        return classes;
     }
 
 }
