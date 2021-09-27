415ea06e97f08a7b1b6a9dc77a784aa87476dc0d
David Aniceto <David.Aniceto@ctw.bmwgroup.com>
2021-09-28 00:32:44 +0100

    testing git tag

--- myPetFinderApp-application/src/main/java/mypetfinderapp/commons/config/JaxRSApplication.java
+++ myPetFinderApp-application/src/main/java/mypetfinderapp/commons/config/JaxRSApplication.java
@@ -1,15 +1,15 @@
-package github.davids13.mypetfinderapp.commons.config;
+package mypetfinderapp;
 
-import github.davids13.mypetfinderapp.commons.errors.CustomExceptionMapper;
-import github.davids13.mypetfinderapp.commons.filterandinterceptors.RequestLoggingFilter;
-import github.davids13.mypetfinderapp.commons.filterandinterceptors.ResponseLoggingFilter;
+import mypetfinderapp.commons.errors.CustomExceptionMapper;
 
+import javax.enterprise.context.ApplicationScoped;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 import java.util.HashSet;
 import java.util.Set;
 
 @ApplicationPath("api")
+@ApplicationScoped
 public class JaxRSApplication extends Application {
 
     // add provider classes here
@@ -17,8 +17,8 @@ public class JaxRSApplication extends Application {
     public Set<Class<?>> getClasses() {
         final Set<Class<?>> classes = new HashSet<>();
         classes.add(CustomExceptionMapper.class);
-        classes.add(RequestLoggingFilter.class);
-        classes.add(ResponseLoggingFilter.class);
+//        classes.add(RequestLoggingFilter.class);
+//        classes.add(ResponseLoggingFilter.class);
         return classes;
     }
 
